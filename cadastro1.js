9f40d227144e36f285e8d55ca8ceb171e3ef86eb
Kaiki <kaiki.machado@portalsesisp.org.br>
2023-06-23 08:40:45 -0300

    stargeek

---
+++ cadastro1.js
@@ -0,0 +1,57 @@
+const formulario = document.getElementById("formulario");
+const msg = document.querySelector(".mensagem")
+const email = document.getElementById("email");
+const senha = document.getElementById("senha"); 
+
+
+function verificarEmail(email, evento){
+    let dados = JSON.parse(localStorage.getItem("bd"));
+    if (dados == null){
+        criarUsuario(evento);
+    } else {
+        dados.forEach(elemento =>{
+            if (elemento.emailcliente == email){
+                msg.innerHTML="E-mail já existe!";
+                evento.preventDefault();
+            } else {
+                criarUsuario(evento);
+            }
+        }
+        );
+    }  
+}
+
+formulario.onsubmit = (evento) =>{
+    if (email.value == ""){
+        evento.preventDefault();
+        msg.innerHTML = "Digite seu E-Mail";
+        email.focus();
+        return null;
+    }
+
+    if (senha.value == ""){
+        evento.preventDefault();
+        msg.innerHTML = "Digite seu Senha";
+        senha.focus();
+        return null;
+    }
+    verificarEmail(email.value, evento);
+}
+
+
+function criarUsuario(evento){
+    let dados = JSON.parse(localStorage.getItem("bd")) || [];
+    dados.push(
+        {
+        emailcliente : email.value,
+        senhacliente : senha.value,
+        }
+    )
+    localStorage.setItem("bd", JSON.stringify(dados));
+    msg.innerHTML ="Usuário Cadastrado com Sucesso";
+    evento.preventDefault();
+    setTimeout(()=>{
+        window.location.assign("login.html");
+},2000)
+    
+}
